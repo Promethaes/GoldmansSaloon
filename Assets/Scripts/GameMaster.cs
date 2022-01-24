2a5df54ec99187f00ce7bbfd47edbf1cde79af9c
Anthony <anth19800@hotmail.com>
2022-01-24 13:30:49 -0500

    [Wip]

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -7,28 +7,44 @@ using UnityEngine.InputSystem;
 public class GameMaster : MonoBehaviour
 {
     public UnityEvent OnAllPlayersDead;
+    public UnityEvent OnStartCounterFinished;
+    public UnityEvent OnBeginGame;
     List<PlayerController> players = new List<PlayerController>();
 
     bool _allPlayersDead = false;
     // Start is called before the first frame update
     void Start()
     {
-        var p = FindObjectsOfType<PlayerController>();
-        foreach (var pl in p)
-            players.Add(pl);
+        void Master_OnBeginGame()
+        {
+            var p = FindObjectsOfType<PlayerController>();
+            foreach (var pl in p)
+                players.Add(pl);
+        }
         void Master_OnAllPlayersDead()
         {
             var entities = FindObjectsOfType<EntityHealth>();
             for (int i = 0; i < entities.Length; i++)
                 Destroy(entities[i].gameObject);
         }
+
+        IEnumerator StartCounter()
+        {
+            yield return new WaitForSeconds(4.0f);
+            OnStartCounterFinished.Invoke();
+            yield return new WaitForSeconds(0.5f);
+            OnBeginGame.Invoke();
+        }
+        StartCoroutine(StartCounter());
+
         OnAllPlayersDead.AddListener(Master_OnAllPlayersDead);
+        OnBeginGame.AddListener(Master_OnBeginGame);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_allPlayersDead)
+        if (_allPlayersDead || players.Count == 0)
             return;
         foreach (var p in players)
         {
---
+++ Assets/Scripts/StartCounter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCounter : MonoBehaviour
+{
+    [SerializeField] float lerpSpeed = 1.0f;
+    [SerializeField] AnimationCurve lerpCurve = null;
+    [SerializeField] Vector3 lerpScale = new Vector3(0.5f, 0.5f, 0.5f);
+    [Header("References")]
+    [SerializeField] TMPro.TextMeshProUGUI text = null;
+    Vector3 _originalScale = Vector2.one;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IEnumerator Counter()
+        {
+            StartCoroutine(Lerp());
+            yield return new WaitForSeconds(1.0f);
+            
+            StartCoroutine(Lerp());
+            text.text = "2";
+            yield return new WaitForSeconds(1.0f);
+            
+            StartCoroutine(Lerp());
+            text.text = "1";
+            yield return new WaitForSeconds(1.0f);
+            
+            StartCoroutine(Lerp());
+            text.text = "GO!!";
+            yield return new WaitForSeconds(1.0f);
+        }
+        IEnumerator Lerp()
+        {
+            float x = 0.0f;
+            while (x < 1.0f)
+            {
+                yield return new WaitForEndOfFrame();
+                x += Time.deltaTime * lerpSpeed;
+                transform.localScale = Vector3.Lerp(lerpScale, _originalScale, lerpCurve.Evaluate(x));
+            }
+        }
+        StartCoroutine(Counter());
+    }
+}
